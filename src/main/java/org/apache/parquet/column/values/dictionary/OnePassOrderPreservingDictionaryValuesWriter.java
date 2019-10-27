16c26b97084d0a7f0dfbc3ecd3092c372cab7edb
Harper <harperjiang@msn.com>
2019-10-27 10:29:02 -0500

    Working on Order Preserving

--- src/main/java/org/apache/parquet/column/values/dictionary/OnePassOrderPreservingDictionaryValuesWriter.java
+++ src/main/java/org/apache/parquet/column/values/dictionary/OnePassOrderPreservingDictionaryValuesWriter.java
@@ -1,9 +1,63 @@
 package org.apache.parquet.column.values.dictionary;
 
+
+import org.apache.parquet.bytes.ByteBufferAllocator;
+import org.apache.parquet.bytes.BytesInput;
+import org.apache.parquet.column.Encoding;
+import org.apache.parquet.column.page.DictionaryPage;
+import org.apache.parquet.column.values.ValuesWriter;
+import org.apache.parquet.io.api.Binary;
+
 /**
  * OnePassOrderPreservingDictionaryValuesWriter generate order-preserving dictionary for each
  * page
  */
 
 public class OnePassOrderPreservingDictionaryValuesWriter {
+
+    public class BinaryDictionaryValuesWriter extends DictionaryValuesWriter.PlainBinaryDictionaryValuesWriter {
+
+        boolean firstPage = true;
+
+        /**
+         * @param maxDictionaryByteSize
+         * @param encodingForDataPage
+         * @param encodingForDictionaryPage
+         * @param allocator
+         */
+        public BinaryDictionaryValuesWriter(int maxDictionaryByteSize, Encoding encodingForDataPage,
+                                            Encoding encodingForDictionaryPage, ByteBufferAllocator allocator) {
+            super(maxDictionaryByteSize, encodingForDataPage, encodingForDictionaryPage, allocator);
+        }
+
+        @Override
+        public BytesInput getBytes() {
+            // This marks the end of a page
+            // Sort the dictionary and re-encode the key
+            sortDictionaryThenRecode();
+            if (firstPage) {
+                firstPage = false;
+            }
+            return super.getBytes();
+        }
+
+        @Override
+        public void writeBytes(Binary v) {
+            if (!firstPage && !binaryDictionaryContent.containsKey(v)) {
+                // Record the newly inserted keys in this round
+
+            }
+            super.writeBytes(v);
+        }
+
+        @Override
+        public DictionaryPage toDictPageAndClose() {
+
+            return super.toDictPageAndClose();
+        }
+
+        protected void sortDictionaryThenRecode() {
+
+        }
+    }
 }
