a58eefbbfe1975a94f1e2885e16eae5b8177796c
harper <harperjiang@msn.com>
2019-10-28 19:59:42 -0500

    Deal with gzip compressed data

--- src/main/java/org/apache/parquet/column/page/CompressedDataPageV2.java
+++ src/main/java/org/apache/parquet/column/page/CompressedDataPageV2.java
@@ -1,9 +1,11 @@
 package org.apache.parquet.column.page;
 
 import org.apache.parquet.bytes.BytesInput;
+import org.apache.parquet.bytes.DirectByteBufferAllocator;
 import org.apache.parquet.io.ParquetEncodingException;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 import static org.apache.parquet.hadoop.CodecFactory.BytesDecompressor;
 
@@ -30,7 +32,10 @@ public class CompressedDataPageV2 extends DataPageV2 {
     public BytesInput getData() {
         try {
             if (compressed) {
-                this.data = decompressor.decompress(this.data, getUncompressedSize());
+                ByteBuffer directBuffer = new DirectByteBufferAllocator().allocate(this.getUncompressedSize());
+                this.decompressor.decompress(this.data.toByteBuffer(), getCompressedSize(),
+                        directBuffer, getUncompressedSize());
+                this.data = BytesInput.from(directBuffer, 0, getUncompressedSize());
                 compressed = false;
             }
             return this.data;
